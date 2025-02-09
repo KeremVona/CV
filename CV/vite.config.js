5041eb559ad4dfee84fc352959bf3943dba8a14d
KeremVona <keremvona@gmail.com>
2025-02-09 21:46:16 +0300

    upload

--- CV/vite.config.js
+++ CV/vite.config.js
@@ -6,3 +6,22 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
+
+
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(), // Use the ES module import
+        autoprefixer(),
+      ],
+    },
+  },
+})*/
